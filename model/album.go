f36af833eba0f2ba871e76f8ebbd3be9955e06e1
Blazej Pachut <bpachut@gmail.com>
2020-04-03 16:59:12 +0200

    go fmt

--- model/album.go
+++ model/album.go
@@ -1,20 +1,20 @@
 package model
 
 type Album struct {
-	ID                    string    `json:"id"`
-	Title                 string    `json:"title"`
-	ProductURL            string    `json:"productUrl"`
-	IsWriteable           bool      `json:"isWriteable"`
-	ShareInfo             ShareInfo `json:"shareInfo"`
-	MediaItemsCount       string    `json:"mediaItemsCount"`
-	CoverPhotoBaseURL     string    `json:"coverPhotoBaseUrl"`
-	CoverPhotoMediaItemID string    `json:"coverPhotoMediaItemId"`
+	ID                    string         `json:"id"`
+	Title                 string         `json:"title"`
+	ProductURL            string         `json:"productUrl"`
+	IsWriteable           bool           `json:"isWriteable"`
+	ShareInfo             AlbumShareInfo `json:"shareInfo"`
+	MediaItemsCount       string         `json:"mediaItemsCount"`
+	CoverPhotoBaseURL     string         `json:"coverPhotoBaseUrl"`
+	CoverPhotoMediaItemID string         `json:"coverPhotoMediaItemId"`
 }
 type SharedAlbumOptions struct {
 	IsCollaborative bool `json:"isCollaborative"`
 	IsCommentable   bool `json:"isCommentable"`
 }
-type ShareInfo struct {
+type AlbumShareInfo struct {
 	SharedAlbumOptions SharedAlbumOptions `json:"sharedAlbumOptions"`
 	ShareableURL       string             `json:"shareableUrl"`
 	ShareToken         string             `json:"shareToken"`
--- model/media_item.go
+++ model/media_item.go
@@ -1,38 +1,38 @@
 package model
 
 type MediaItem struct {
-	ID            string `json:"id"`
-	Description   string `json:"description"`
-	ProductURL    string `json:"productUrl"`
-	BaseURL       string `json:"baseUrl"`
-	MimeType      string `json:"mimeType"`
-	MediaMetadata MediaMetadata `json:"mediaMetaData"`
+	ID              string          `json:"id"`
+	Description     string          `json:"description"`
+	ProductURL      string          `json:"productUrl"`
+	BaseURL         string          `json:"baseUrl"`
+	MimeType        string          `json:"mimeType"`
+	MediaMetadata   MediaMetadata   `json:"mediaMetaData"`
 	ContributorInfo ContributorInfo `json:"contributorInfo"`
-	Filename string `json:"filename"`
+	Filename        string          `json:"filename"`
 }
 
 type MediaMetadata struct {
-	CreationTime string `json:"creationTime"`
-	Width        string `json:"width"`
-	Height       string `json:"height"`
-	PhotoMetadata     PhotoMetadata `json:"photo,omitempty"`
-	VideoMetadata     VideoMetadata `json:"video,omitempty"`
+	CreationTime  string        `json:"creationTime"`
+	Width         string        `json:"width"`
+	Height        string        `json:"height"`
+	PhotoMetadata PhotoMetadata `json:"photo,omitempty"`
+	VideoMetadata VideoMetadata `json:"video,omitempty"`
 }
 
 type PhotoMetadata struct {
-	CameraMake      string `json:"cameraMake"`
-	CameraModel     string `json:"cameraModel"`
-	FocalLength     float32    `json:"focalLength"`
-	ApertureFNumber float32    `json:"apertureFNumber"`
-	IsoEquivalent   int    `json:"isoEquivalent"`
-	ExposureTime    string `json:"exposureTime"`
+	CameraMake      string  `json:"cameraMake"`
+	CameraModel     string  `json:"cameraModel"`
+	FocalLength     float32 `json:"focalLength"`
+	ApertureFNumber float32 `json:"apertureFNumber"`
+	IsoEquivalent   int     `json:"isoEquivalent"`
+	ExposureTime    string  `json:"exposureTime"`
 }
 
 type VideoMetadata struct {
-	CameraMake  string `json:"cameraMake"`
-	CameraModel string `json:"cameraModel"`
-	Fps         float32    `json:"fps"`
-	Status      string `json:"status"`
+	CameraMake  string  `json:"cameraMake"`
+	CameraModel string  `json:"cameraModel"`
+	Fps         float32 `json:"fps"`
+	Status      string  `json:"status"`
 }
 type ContributorInfo struct {
 	ProfilePictureBaseURL string `json:"profilePictureBaseUrl"`
